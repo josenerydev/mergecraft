700a5b38683141e3064397b8528cc95487049a64
Jose Nery <josenerydev@gmail.com>
2023-08-18 20:58:44 -0300

    feat(mergecraft): refactor find_subdir and add should_skip_directory
    This commit refactors the find_subdir function in mergecraft.py to improve its efficiency. It now uses a nested loop to check for the existence of the subdir_name in any of the directory names. If found, it returns the full path of the directory. Otherwise, it prints a debug message and returns None.
    
    Additionally, a new function called should_skip_directory has been added. This function checks if a directory should be skipped based on specific criteria. It examines the directory path for the presence of any of the specified skip directories (bin, obj, .git).

--- mergecraft/mergecraft.py
+++ mergecraft/mergecraft.py
@@ -9,8 +9,13 @@ import re
 def find_subdir(start_dir, subdir_name):
     """Recursively search for a subdirectory within the start directory."""
     for dirpath, dirnames, filenames in os.walk(start_dir):
-        if os.path.basename(dirpath) == subdir_name:
-            return dirpath
+        for dirname in dirnames:
+            if subdir_name in dirname:
+                print(f"Found directory: {os.path.join(dirpath, dirname)}")  # Debug
+                return os.path.join(dirpath, dirname)
+    print(
+        f"Directory '{subdir_name}' not found from starting point: {start_dir}"
+    )  # Debug
     return None
 
 
@@ -29,36 +34,57 @@ def load_gitignore():
     return pathspec.PathSpec.from_lines("gitwildmatch", gitignore)
 
 
+def should_skip_directory(dirpath):
+    """Return True if the directory should be skipped based on certain criteria."""
+    skip_directories = ["bin", "obj", ".git"]
+    for skip_dir in skip_directories:
+        if skip_dir in dirpath.split(os.path.sep):
+            return True
+    return False
+
+
 def main():
     # Parse command-line arguments
     parser = argparse.ArgumentParser(
         description="Merge files into a temporary file and open in VS Code."
     )
-
-    ext_help = """
-    Specify the file extensions to process. 
-    You can provide multiple extensions by separating them with spaces.
-    Example:
-        mergecraft -e .py .txt
-        This will process both Python and TXT files.
-    If not provided, the tool defaults to processing .py files.
-    """
     parser.add_argument(
-        "-e", "--extensions", nargs="+", default=[".py"], help=ext_help.strip()
+        "-e",
+        "--extensions",
+        nargs="+",
+        default=None,  # Default para None, vamos tratar isso mais adiante
+        help="Specify the file extensions to process.",
     )
-
     parser.add_argument(
         "--path",
         default=".",
         help="Specify the root path to start searching for files. Defaults to current directory.",
     )
-
     parser.add_argument(
         "--filter",
         help="A regex pattern to filter files based on their content or name. Only files matching this pattern will be included.",
     )
-
     args = parser.parse_args()
+
+    # Se --path for fornecido e --extensions não for, leremos todos os arquivos
+    if args.path != "." and args.extensions is None:
+        args.extensions = [""]
+    elif (
+        args.extensions is None
+    ):  # Se --extensions não for fornecido, vamos usar .py como padrão
+        args.extensions = [".py"]
+
+    # Validate Regex
+    if args.filter:
+        try:
+            re.compile(args.filter)
+        except re.error:
+            print("Error: Invalid regular expression provided!")
+            return
+
+    # If no extensions are explicitly provided, default to .py
+    if args.extensions is None:
+        args.extensions = [".py"]
 
     # Normalize the path and check if it exists
     current_dir = os.getcwd()
@@ -83,12 +109,27 @@ def main():
     total_line_count = 0
 
     for dirpath, dirnames, filenames in os.walk(full_path):
+        # Debug
+        print(f"Checking directory: {dirpath}")
+
+        # Ignore the .git directory
+        if ".git" in dirpath:
+            print("Skipping .git directory")  # Debug
+            continue
+
         for filename in filenames:
+            # Filter by extensions
+            if not filename.endswith(tuple(args.extensions)):
+                continue
+
             filepath = os.path.join(dirpath, filename)
             relative_filepath = os.path.relpath(filepath)
 
+            print(f"Checking file: {filepath}")  # Debug
+
             # Check against gitignore rules
             if gitignore_spec and gitignore_spec.match_file(relative_filepath):
+                print(f"File {filepath} is ignored due to .gitignore rules.")  # Debug
                 continue  # Skip this file
 
             # Apply the filter to both filename and content
@@ -96,6 +137,7 @@ def main():
             if args.filter and not (
                 re.search(args.filter, content) or re.search(args.filter, filename)
             ):
+                print(f"File {filepath} does not match the provided filter.")  # Debug
                 continue
 
             line_count = len(content.split("\n"))
@@ -113,7 +155,9 @@ def main():
         return
 
     print("Editing completed. Continuing script execution...")
-
+    print("\nSummary:")
+    print(f"Total files read: {len(read_files)}")
+    print(f"Total lines: {total_line_count}")
     print("\nFiles read:")
     for file_path, line_count in read_files:
         print(f"{file_path}: {line_count} lines")
