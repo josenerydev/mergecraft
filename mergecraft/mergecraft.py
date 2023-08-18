e2f2d197db904eae91a7c4cf4daa349d77e48835
Jose Nery <josenerydev@gmail.com>
2023-08-18 18:47:22 -0300

    feat: update mergecraft.py for command-line argument parsing
    The main function of mergecraft.py has been modified to include command-line argument parsing using the argparse module. It now creates an argument parser and adds the -e or --extensions argument, allowing users to specify file extensions to process. The default extension is set to .py.
    
    The script now reads the specified file extensions provided as arguments and adds them to the list of read files. Previously, it only processed .py files.
    
    This commit improves the versatility of mergecraft.py by enabling users to customize the file extensions to be processed.

--- mergecraft/mergecraft.py
+++ mergecraft/mergecraft.py
@@ -2,6 +2,7 @@ import os
 import tempfile
 import subprocess
 import pathspec
+import argparse
 
 
 def read_file_content(filepath):
@@ -19,6 +20,22 @@ def load_gitignore():
 
 
 def main():
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description="Merge files into a temporary file and open in VS Code.")
+    
+    # Detailed help message for the extensions argument
+    ext_help = """
+    Specify the file extensions to process. 
+    You can provide multiple extensions by separating them with spaces.
+    Example:
+        mergecraft -e .py .txt
+        This will process both Python and TXT files.
+    If not provided, the tool defaults to processing .py files.
+    """
+    
+    parser.add_argument("-e", "--extensions", nargs='+', default=['.py'], help=ext_help.strip())
+    args = parser.parse_args()
+
     # Create a temporary file
     temp_fd, temp_path = tempfile.mkstemp(suffix=".md")
     os.close(temp_fd)  # Close the file descriptor, we'll use the path directly
@@ -35,15 +52,14 @@ def main():
     for dirpath, dirnames, filenames in os.walk("."):
         for filename in filenames:
             filepath = os.path.join(dirpath, filename)
-            relative_filepath = os.path.relpath(
-                filepath
-            )  # get path relative to current directory
+            relative_filepath = os.path.relpath(filepath)  # get path relative to current directory
 
             # Check against gitignore rules
             if gitignore_spec and gitignore_spec.match_file(relative_filepath):
                 continue  # Skip this file
 
-            if filename.endswith(".py"):
+            # Check if file matches specified extensions
+            if any(filename.endswith(ext) for ext in args.extensions):
                 # Add the file to the list of read files
                 read_files.append(relative_filepath)
 
@@ -55,6 +71,11 @@ def main():
 
     # Open the temporary file in VS Code
     subprocess.run(["cmd", "/c", "code", "-w", temp_path])
+
+    # Check if no files were read
+    if not read_files:
+        print("No files were read!")
+        return
 
     print("Editing completed. Continuing script execution...")
 
